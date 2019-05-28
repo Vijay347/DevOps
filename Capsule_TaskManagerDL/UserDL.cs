107fdecb155c3b658ff67a206043766a405a238d
584684 <584684@PC375161>
2019-05-28 15:15:18 +0530

    First Commit

---
+++ Capsule_TaskManagerDL/UserDL.cs
@@ -0,0 +1,68 @@
+﻿using Capsule_TaskManager.Models.User;
+using Capsule_TaskManagerDL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capsule_TaskManagerDL
+{
+    public class UserDL
+    {
+        #region GetUserDetails
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<UserModel> GetUserDetails()
+        {
+            List<UserModel> userModelList = null;
+            using (TaskManagerEntities db = new TaskManagerEntities())
+            {
+                var userList = db.GET_USER_DETAILS();
+
+                if (userList != null)
+                {
+                    userModelList = new List<UserModel>();
+
+                    foreach (var item in userList)
+                    {
+                        userModelList.Add(new UserModel()
+                        {
+                            User_ID = item.User_ID,
+                            First_Name = item.First_Name,
+                            Last_Name = item.Last_Name,
+                            Employee_ID = item.Employee_ID
+                        });
+                    }
+                }
+
+                return userModelList;
+            }
+        }
+        #endregion
+
+        #region Insert/Update User Detail
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="taskModel"></param>
+        /// <returns></returns>
+
+        public bool SubmitUserDetail(UserModel userModel)
+        {
+            var isSubmitted = false;
+
+            using (TaskManagerEntities objEntities = new TaskManagerEntities())
+            {
+                var existingTask = objEntities.INSERT_USER_DETAILS(userModel.User_ID, userModel.First_Name, userModel.Last_Name, userModel.Employee_ID, userModel.Project_ID, userModel.Task_ID, userModel.Action);
+                var updatedRecord = objEntities.SaveChanges();
+                isSubmitted = existingTask > 0;
+            }
+
+            return isSubmitted;
+        }
+    }
+    #endregion
+}
---
+++ Nunit_Capsule_TaskManager.Tests/Controllers/NUnit_UserController.cs
@@ -0,0 +1,70 @@
+﻿using Capsule_TaskManager.Controllers;
+using Capsule_TaskManager.Models.User;
+using NUnit.Framework;
+
+namespace Nunit_Capsule_TaskManager.Tests.Controllers
+{
+    [TestFixture]
+    public class NUnit_UserController
+    {
+        #region Public Declaration
+
+        UserController objUserController = new UserController();
+
+        #endregion
+
+        #region GetUserDetails
+        /// <summary>
+        /// To get User details
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public void GetUserDetails()
+        {
+            var vlsit = objUserController.GetUserDetails();
+            Assert.IsTrue(vlsit != null);
+        }
+        #endregion
+
+        #region SubmitUserDetail
+        /// <summary>
+        /// Insert the User details which user entered
+        /// </summary>
+        /// <param name="objGET_User_DETAILS_Result"></param>
+        /// <returns></returns>
+        [Test, Order(1)]
+        public void InsertUserDetail()
+        {
+            UserModel userModel = new UserModel();
+            userModel.User_ID = 0;
+            userModel.First_Name = "Vijay";
+            userModel.Last_Name = "Palani";
+            userModel.Employee_ID = 1;
+            userModel.Action = "insert";
+            userModel.Project_ID = 1;
+            userModel.Task_ID = 1;
+
+            var isInserted = objUserController.SubmitUserDetail(userModel);
+            Assert.IsTrue(isInserted);
+        }
+
+        [Test]
+        public void UpdateUserDetail()
+        {
+            UserModel userModel = new UserModel();
+            userModel.User_ID = 1;
+            userModel.First_Name = "Vijayakumar";
+            userModel.Last_Name = "Palani";
+            userModel.Employee_ID = 1;
+            userModel.Action = "update";
+            userModel.Project_ID = 1;
+            userModel.Task_ID = 1;
+
+            var isUpdated = objUserController.SubmitUserDetail(userModel);
+            Assert.IsTrue(isUpdated);
+        }
+        #endregion
+
+        
+    }
+}
